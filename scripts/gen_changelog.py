de7fa18ca47f8f0a1ea4d1dfaafcd6ea0f0f5a3b
Martin Hjelmare <marhje52@kth.se>
2018-04-05 20:06:53 +0200

    Create python script to generate changelog

---
+++ scripts/gen_changelog.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""Generate changelog."""
+import os
+
+from pygcgen.main import ChangelogGenerator
+
+
+def validate_version():
+    """Validate version before release."""
+    import leicacam
+    version_string = leicacam.__version__
+    versions = version_string.split('.', 3)
+    try:
+        for ver in versions:
+            int(ver)
+    except ValueError:
+        print(
+            'Only integers are allowed in release version, '
+            'please adjust current version {}'.format(version_string))
+        return None
+    return version_string
+
+
+def generate():
+    """Generate changelog."""
+    old_dir = os.getcwd()
+    proj_dir = os.path.join(os.path.dirname(__file__), os.pardir)
+    os.chdir(proj_dir)
+    version = validate_version()
+    if not version:
+        os.chdir(old_dir)
+        return
+    print('Generating changelog for version {}'.format(version))
+    options = [
+        '--user', 'arve0', '--project', 'leicacam', '-v', '--with-unreleased',
+        '--future-release', version]
+    generator = ChangelogGenerator(options)
+    generator.run()
+    os.chdir(old_dir)
+
+
+if __name__ == '__main__':
+    generate()
