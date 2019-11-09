5421c03513aec596b85cd2c9309d0a68d82775a2
Martin Hjelmare <marhje52@kth.se>
2019-11-09 15:07:23 +0100

    Run black

--- async_client.py
+++ async_client.py
@@ -9,18 +9,18 @@ async def run(loop):
     cam = AsyncCAM(loop=loop)
     await cam.connect()
     print(cam.welcome_msg)
-    await cam.send(b'/cmd:deletelist')
+    await cam.send(b"/cmd:deletelist")
     print(await cam.receive())
-    await cam.send(b'/cmd:deletelist')
-    print(await cam.wait_for(cmd='cmd', timeout=0.1))
-    await cam.send(b'/cmd:deletelist')
-    print(await cam.wait_for(cmd='cmd', timeout=0))
-    print(await cam.wait_for(cmd='cmd', timeout=0.1))
-    print(await cam.wait_for(cmd='test', timeout=0.1))
+    await cam.send(b"/cmd:deletelist")
+    print(await cam.wait_for(cmd="cmd", timeout=0.1))
+    await cam.send(b"/cmd:deletelist")
+    print(await cam.wait_for(cmd="cmd", timeout=0))
+    print(await cam.wait_for(cmd="cmd", timeout=0.1))
+    print(await cam.wait_for(cmd="test", timeout=0.1))
     cam.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     LOOP = asyncio.new_event_loop()
     LOOP.run_until_complete(run(LOOP))
     LOOP.close()
--- leicacam/__init__.py
+++ leicacam/__init__.py
@@ -1,9 +1,10 @@
 """Control Leica microscopes with python."""
-__author__ = 'Arve Seljebu'
-__email__ = 'arve.seljebu@gmail.com'
+__author__ = "Arve Seljebu"
+__email__ = "arve.seljebu@gmail.com"
 from os.path import join, dirname
-__version__ = open(join(dirname(__file__), 'VERSION')).read().strip()
 
-__all__ = ['CAM']
+__version__ = open(join(dirname(__file__), "VERSION")).read().strip()
+
+__all__ = ["CAM"]
 
 from .cam import CAM
