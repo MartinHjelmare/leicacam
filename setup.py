25e6d4d6f2ac30ea2b6c6f1e35ee8c85fa7e069a
Martin Hjelmare <marhje52@gmail.com>
2020-06-10 13:57:01 +0200

    Set development status to beta

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     zip_safe=False,
     keywords="leicacam",
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
